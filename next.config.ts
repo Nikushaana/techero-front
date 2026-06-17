3b613b156aeaa635d94e834a215ee008f1d8d988
Nikushaana <nikanashvili@gmail.com>
2026-06-17 22:28:20 +0400

    cookies auth corrected

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       destination: 'https://techero.ge/api/:path*',
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.techero.ge/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
